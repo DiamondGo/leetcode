19de86dc1fb8ee58c0a1bfe3b4412dda9dfa5db8
Kenneth Tse <xie.kenneth@gmail.com>
2015-04-18 16:56:01 +0800

    aaaadd

---
+++ python/LinkedListCycle.py
@@ -0,0 +1,56 @@
+'''
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+'''
+from tkinter.tix import ListNoteBook
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        if head is None:
+            return False
+        n = 1
+        tail = head
+        while True:
+            ###
+            cur = head
+            for i in range(n):
+                if tail.next == cur:
+                    return True
+                cur = cur.next
+            n += 1
+            tail = tail.next
+            if tail is None:
+                return False
+            
+
+        
+        
+
+if __name__ == '__main__':
+    class ListNode:
+         def __init__(self, x):
+             self.val = x
+             self.next = None
+             
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l3 = ListNode(3)
+    l4 = ListNode(4)
+    
+    l1.next = l1
+    l2.next = l3
+    l3.next = l4
+    #l4.next = l4
+    
+    print(Solution().hasCycle(l1))
+    
