88669aa0c26fd81bfc37fc687e699512af8aa643
Kenneth Tse <xie.kenneth@gmail.com>
2016-08-19 23:28:11 +0800

    add some new answer

---
+++ golang/InvertBinaryTree.go
@@ -0,0 +1,45 @@
+package golang
+
+/*
+nvert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+    return root
+}
+
---
+++ golang/TwoSumII_InputArrayIsSorted.go
@@ -0,0 +1,33 @@
+package golang
+
+import "fmt"
+/*
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+ */
+
+
+func twoSum(numbers []int, target int) []int {
+	s, e := 0, len(numbers) - 1
+	for s < e {
+		sum := s + e
+		if sum < target {
+			s++
+		} else if sum > target {
+			e--
+		} else if sum == target {
+			return []int{s+1, e+1}
+		}
+	}
+	return []int{0, 0}
+}
+
+func main() {
+	fmt.Println("hello")
+}
