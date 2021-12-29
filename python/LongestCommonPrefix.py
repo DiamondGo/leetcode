fcccd9d0abe6e36e04f74cd12327c86831eca652
DiamondGo <xie.kenneth@gmail.com>
2021-12-28 20:49:02 -0800

    Add answer LCP

--- python/LongestCommonPrefix.py
+++ python/LongestCommonPrefix.py
@@ -1,14 +1,15 @@
 '''
-Created on 2015Äê8ÔÂ6ÈÕ
+Created on 2015ï¿½ï¿½8ï¿½ï¿½6ï¿½ï¿½
 
 @author: Kenneth Tse
 '''
-from _overlapped import NULL
+#from _overlapped import NULL
 
 '''
 Write a function to find the longest common prefix string amongst an array of strings.
 '''
 
+"""
 class Solution:
     # @param {string[]} strs
     # @return {string}
@@ -29,8 +30,25 @@ class Solution:
             idx += 1
             if idx >= len(strs[0]):
                 return strs[0]
+"""
+from typing import List
 
-            
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        index: int = 0
+        if len(strs) == 1:
+            return strs[0]
+        breakouterloop = False
+        while index < len(strs[0]) and not breakouterloop:
+            for i in range(1, len(strs)):
+                if index >= len(strs[i]) or strs[0][index] != strs[i][index]:
+                    breakouterloop = True
+                    break
+            if not breakouterloop:
+                index += 1       
+        return strs[0][0:index]
 
 if __name__ == '__main__':
-    pass
+    input = ["flower","flow","flight"]
+    s = Solution()
+    print(s.longestCommonPrefix(input))
